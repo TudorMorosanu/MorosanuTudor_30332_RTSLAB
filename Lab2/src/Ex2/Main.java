f44659fa531200a1fd2aa9affcb897ef32907ebc
TudorMorosanu <113435664+beautifulyautja@users.noreply.github.com>
2024-03-03 17:21:42 +0200

    FInished Ex2 for lab2

---
+++ Lab2/src/Ex2/Main.java
@@ -0,0 +1,13 @@
+package Ex2;
+
+public class Main {
+
+    private static final int noOfThreads=6;
+
+    private static final int processorLoad=1000000;
+
+    public static void main(String[] args){
+
+        new Controller().start(noOfThreads, processorLoad);
+    }
+}
---
+++ Lab2/src/Ex2/UpdateObserver.java
@@ -0,0 +1,27 @@
+package Ex2;
+
+public class UpdateObserver {
+    public int id;
+    public int progress;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProgress(int progress) {
+        this.progress = progress;
+    }
+
+    public UpdateObserver(int id, int progress) {
+        this.id = id;
+        this.progress = progress;
+    }
+}
