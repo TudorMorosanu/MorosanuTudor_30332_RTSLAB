4b82ca29ba71fe9b3b426b3f1d1cac0a0e041309
unknown <tudormorosanu20@gmail.com>
2024-04-02 18:46:32 +0300

    test commit

--- Lab4/src/Ex5/Main.java
+++ Lab4/src/Ex5/Main.java
@@ -10,6 +10,7 @@ public class Main {
         Integer lock1 = new Integer(1);
         Integer lock2 = new Integer(1);
 
+        // test comment to see that repo is ok
         ExecutionThread1 thread1 = new ExecutionThread1(lock1, lock2, t1);
         ExecutionThread2 thread2 = new ExecutionThread2(lock1, t2);
         ExecutionThread2 thread3 = new ExecutionThread2(lock2, t3);
