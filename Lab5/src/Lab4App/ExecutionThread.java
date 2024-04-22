8623d169ac6e6d9b4136ee32a63483a69d626b2d
TudorMorosanu <tudormorosanu20@gmail.com>
2024-04-22 13:26:50 +0300

    implementation of lab6, corrected lab5

--- Lab5/src/Lab4App/ExecutionThread.java
+++ Lab5/src/Lab4App/ExecutionThread.java
@@ -3,17 +3,17 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ExecutionThread extends Thread {
 
-    final ReentrantLock monitor1, monitor2, monitorSh;
+    final ReentrantLock monitor1, monitor2;
     int[] times;
 
-    public ExecutionThread(ReentrantLock monitor1, ReentrantLock monitor2, ReentrantLock monitorSh, int[] times) {
+    public ExecutionThread(ReentrantLock monitor1, ReentrantLock monitor2, int[] times) {
         this.monitor1 = monitor1;
         this.monitor2 = monitor2;
-        this.monitorSh = monitorSh;
         this.times = times;
     }
 
-    public void run() {
+    public void run()
+    {
         System.out.println(this.getName() + " - STATE 1");
         int k = (int) Math.round(Math.random() * (times[1] - times[0]) + times[0]);
         for (int i = 0; i < k * 100000; i++) {
@@ -21,7 +21,6 @@ public class ExecutionThread extends Thread {
             i--;
         }
 
-        monitorSh.lock();
         monitor1.lock();
 
         System.out.println(this.getName() + " - STATE 2");
@@ -30,17 +29,20 @@ public class ExecutionThread extends Thread {
             i++;
             i--;
         }
-        monitor2.lock();
-        monitorSh.unlock();
-
-        System.out.println(this.getName() + " - STATE 3");
-        try {
-            Thread.sleep((long) times[4] * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if(monitor2.tryLock())
+        {
+            System.out.println(this.getName() + " - STATE 3");
+            try {
+                Thread.sleep((long)times[4] * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                monitor2.unlock();
+            }
         }
+
         monitor1.unlock();
-        monitor2.unlock();
         System.out.println(this.getName() + " - STATE 4");
     }
 }
